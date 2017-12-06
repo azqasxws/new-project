189c11f860cca52a91b44b0417ef5e3a934d2976
linzixi31 <912379914@qq.com>
2017-12-06 22:55:29 +0800

    基本架构

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vrouter from 'vue-router';
 import Logincomponent from '../components/login/login.vue';
+import homecomponent from '../components/home/home.vue';
+import minecomponent from '../components/mine/mine.vue';
 
 
 Vue.use(Vrouter);
@@ -9,9 +11,18 @@ var router  = new Vrouter({
     routes : [
         {
             path:"/login",
-            component: Logincomponent,
+            component: Logincomponent
 
-        }    
+        },   
+        {
+            path:'/home',
+            component: homecomponent
+        },
+        {
+            path:'/mine',
+            component: minecomponent
+        },
+
 
     ]
 })
