eca794095b124d19c10a661dd4335641cc3b5fa7
linzixi31 <912379914@qq.com>
2017-12-07 09:30:32 +0800

    上传到master

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,33 @@
 <template>
   <div id='app'>
-  <div class="page-group">
+    <div class="page-group">
         <div class="page page-current">
         <!-- 你的html代码 -->
                 <header class="bar bar-nav">
-                <a class="button button-link button-nav pull-left" href="/demos/card" data-transition='slide-out'>
-                <span class="icon icon-left"></span>
-                返回
-                </a>
-                <h1 class="title">我的生活</h1>
+                  <h1 class="title">首页</h1>
                 </header>
                 <nav class="bar bar-tab">
-                <a class="tab-item active" href="#">
-                <span class="icon icon-home"></span>
-                <span class="tab-label">首页</span>
-                </a>
-                <a class="tab-item" href="#">
-                <span class="icon icon-me"></span>
-                <span class="tab-label">我</span>
-                </a>
-                <a class="tab-item" href="#">
-                <span class="icon icon-star"></span>
-                <span class="tab-label">收藏</span>
-                </a>
-                <a class="tab-item" href="#">
-                <span class="icon icon-settings"></span>
-                <span class="tab-label">设置</span>
-                </a>
+                  <a class="tab-item active" @click="home">
+                    <span class="icon icon-home"></span>
+                    <span class="tab-label">首页</span>
+                  </a>
+                  <a class="tab-item" @click="barcode">
+                    <span class="icon icon-star"></span>
+                    <span class="tab-label">扫码购</span>
+                  </a>
+                  <a class="tab-item" @click="cart">
+                    <span class="icon icon-cart"></span>
+                    <span class="tab-label">购物车</span>
+                  </a>
+                  <a class="tab-item" @click="mine">
+                    <span class="icon icon-me"></span>
+                    <span class="tab-label">我的</span>
+                  </a>
                 </nav>
                 <div class="content">
                 <!-- 这里是页面内容区 -->
 
-                <router-view></router-view>
+                  <router-view></router-view>
                 </div>
         </div>
     </div>
@@ -45,13 +41,27 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods:{
+        home : function(){
+                this.$router.push({path: '/home'})
+            },
+        mine : function(){
+              this.$router.push({path:'/mine'}) 
+        },
+        cart : function(){
+              this.$router.push({path:'/cart'}) 
+        },
+        barcode:function(){
+              this.$router.push({path:'/barcode'}) 
+        }
   }
 }
 </script>
 
 <style>
 @import url("//g.alicdn.com/msui/sm/0.6.2/css/sm.min.css");
- #app{
-  height: 100%;
- }
+.bar{
+  background: #fff;
+}
 </style>
---
+++ src/components/mine/mine.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="content">
+      <div class="list-block">
+        <ul>
+          <li class="item-content">
+            <div class="item-media"><i class="icon icon-f7"></i></div>
+            <div class="item-inner">
+              <div class="item-title">商品名称</div>
+              <div class="item-after">杜蕾斯</div>
+            </div>
+          </li>
+          <li class="item-content">
+            <div class="item-media"><i class="icon icon-f7"></i></div>
+            <div class="item-inner">
+              <div class="item-title">型号</div>
+              <div class="item-after">极致超薄型</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    
+</template>
+<script type="text/javascript">
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            
+
+        }
+
+
+    }
+
+</script>
