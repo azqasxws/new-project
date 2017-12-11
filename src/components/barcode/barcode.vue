8074b30deea2eb93ea0742774b15832b4308a9c3
azqasxws <121110350@qq.com>
2017-12-11 09:03:46 +0800

    aa

--- src/components/barcode/barcode.vue
+++ src/components/barcode/barcode.vue
@@ -1,47 +1,18 @@
 <template>
-    <div><h1>扫码</h1></div>
+    <div class="content">
+       
+        <div id="bcid" style="background-color: #000000;">
+            <div style="height:40%"></div>
+            <p class="tip">...载入中...</p>
+            </div>
+            <footer>
+                <div class="fbt">取　 消</div>
+            </footer>
+    </div>
 </template>
-<script type="text/javascript">
-    import axios from 'axios';
-    import qs from 'qs';
-    import jwt from 'jsonwebtoken';
-    export default{
-        data:function(){
-            return {
-                toolbar:null,
-                token:''
-            }
-        },
-        methods: {
-            login:function(){
-                  this.$router.push({path:'/login'}) 
-            }
-        },
-        beforeCreate:function(){    
-            if($.cookie('token')){
-               this.token=$.cookie('token'); 
-               jwt.verify(this.token,'abc',function(error,result){
-                    axios({
-                            url: 'http://localhost:777/php/barcode.php',
-                            method: 'post',
-                            data: qs.stringify({username:result.username}),
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded'
-                            }
-                    }).then(res => {
-                                    if(res.data=="ok"){
-                                        console.log(res.data);
-                                    }else{
-                                        this.$router.push({path:'/login'}); 
-                                    }
-                            });
-                });
-               
-            }else{
-               this.$router.push({path:'/login'}); 
-            }
-                
-        }
 
-    }
+<script type="text/javascript">
+        
+    
+
 </script>
